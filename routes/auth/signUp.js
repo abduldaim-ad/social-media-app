3b2617b70745fb9e4ef30951b4f7dac08924965a
abduldaim-ad <gdaim551@gmail.com>
2023-08-18 03:22:17 +0500

    Username and Password

--- routes/auth/signUp.js
+++ routes/auth/signUp.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs')
 const User = require('../../models/user');
 const router = express.Router();
+const requireLogin = require('../../middlewares/requireLogin')
 
 router.post('/signup', (req, res) => {
     console.log(req.body)
@@ -39,5 +40,59 @@ router.post('/signup', (req, res) => {
             res.json({ err: `Error While Signing Up ${err}` });
         });
 });
+
+router.put('/updateusername', requireLogin, async (req, res) => {
+    const { _id, username } = req.body;
+    if (!username) {
+        return res.status(422).json({ err: "Please Fill All the Fields!" })
+    }
+    const user = await User.findById(_id);
+    if (user) {
+        const update = await User.updateOne({ _id: user._id }, { username })
+        if (update) {
+            return res.status(200).json({ msg: "Username Updated Successfully!" })
+        }
+        else {
+            return res.status(500).json({ err: "Error While Updating Username!" })
+        }
+    }
+    else {
+        return res.status(404).json({ err: "User Not Found!" })
+    }
+})
+
+router.put('/updatepassword', requireLogin, async (req, res) => {
+    const { _id, oldPassword, password, cPassword } = req.body;
+    if (!oldPassword || !password || !cPassword) {
+        return res.status(422).json({ err: "Please Fill All the Fields!" });
+    }
+    const user = await User.findById(_id);
+    if (user) {
+        const compare = await bcrypt.compare(oldPassword, user.password);
+        if (compare) {
+            if (password !== cPassword) {
+                return res.status(422).json({ err: "Passwords Did Not Match!" });
+            }
+            else {
+                const hashedPassword = await bcrypt.hash(password, 12);
+                if (hashedPassword) {
+                    const update = await User.updateOne({ _id: user._id }, { password: hashedPassword })
+                    if (update) {
+                        return res.status(200).json({ msg: "Password Updated Successfully!" })
+                    }
+                    else {
+                        return res.status(500).json({ err: "Error While Updating Password!" })
+                    }
+                }
+            }
+        }
+        else {
+            return res.status(401).json({ err: "Invalid Current Password!" });
+        }
+    }
+    else {
+        return res.status(404).json({ err: "User Not Found!" })
+    }
+})
 
 module.exports = router;
